361d5dbad3cfe25e5c5690b346048a14730f7d88
sod-sherman <sod.sherman@gmail.com>
2020-07-03 12:41:48 -0500

    Lab03-01-mongo

---
+++ GreetingService-Mongo/src/main/java/greet/GreetingServiceMongoApplication.java
@@ -0,0 +1,13 @@
+package greet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingServiceMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingServiceMongoApplication.class, args);
+	}
+
+}
---
+++ GreetingService-Mongo/src/test/java/greet/GreetingServiceMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package greet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingServiceMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
